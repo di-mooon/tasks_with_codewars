c9de07eabb2523d021d064271b101399136647fe
krivosheind28 <78710344+krivosheind28@users.noreply.github.com>
2021-08-29 17:55:54 +0300

    1

--- другие задачи/3ку/Battleship field validator.py
+++ другие задачи/3ку/Battleship field validator.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 
+# можно было бы и лучше, но это работает
+
 def find_ships(field):
     lst = []
     lst2 = []
     for row in range(12):
         for col in range(12):
             if field[row][col] == 1 \
-                    and (field[row + 1][col] == 0 or field[row + 1][col] == 2)\
-                    and (field[row - 1][col] == 0 or field[row - 1][col] == 2)\
+                    and (field[row + 1][col] == 0 or field[row + 1][col] == 2) \
+                    and (field[row - 1][col] == 0 or field[row - 1][col] == 2) \
                     and (field[row - 1][col - 1] == 0 or field[row - 1][col - 1] == 2) \
                     and (field[row - 1][col + 1] == 0 or field[row - 1][col + 1] == 2) \
                     and (field[row + 1][col + 1] == 0 or field[row + 1][col + 1] == 2) \
@@ -22,7 +24,7 @@ def find_ships(field):
 
 def validate_battlefield(field):
     correct_lst = [1] * 8 + [2, 2, 2, 3, 3, 4]
-    field = np.array([[2]*12]+[[2]+x+[2] for x in field]+[[2]*12])
+    field = np.array([[2] * 12] + [[2] + x + [2] for x in field] + [[2] * 12])
     return sorted([len(ships) for ships in (find_ships(field) + find_ships(field.T))]) == correct_lst
 
 
